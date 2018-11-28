725d7e16ff20eeebce9c393a2eed79a6a059abff
satish <satish.appanasamy@gmail.com>
2018-11-28 11:53:00 +0800

    - Link Axios to Ducks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import logo from '../logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
 
